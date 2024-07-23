a431cb4ed4d705026bd4f4614b0ea0e9671e6384
Thiago Silva <titusilvs91@gmail.com>
2024-07-22 23:11:41 -0300

    Resumo Vendas

--- src/Pages/Resumo.tsx
+++ src/Pages/Resumo.tsx
@@ -1,9 +1,42 @@
-import React from 'react'
+import { useData } from "../Context/DataContext";
 
 const Resumo = () => {
+  const { data } = useData();
+  if (data === null) return null;
   return (
-    <div>Resumo</div>
-  )
-}
+    <section>
+      <div className="resumo flex mb">
+        <div className="box">
+          <h2>Vendas</h2>
+          <span>
+            {data.filter(({status}) => status !== "falha")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
 
-export default Resumo
+        <div className="box">
+          <h2>Recebido</h2>
+          <span>
+            {data.filter(({status}) => status === "pago")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+
+        <div className="box">
+          <h2>Processando</h2>
+          <span>
+            {data.filter(({status}) => status === "processando")
+              .reduce((acc, item) => acc + item.preco, 0)
+              .toLocaleString("pt-br", { style: "currency", currency: "BRL" })}
+          </span>
+        </div>
+      </div>
+
+      <div className="box">Gráficos</div>
+    </section>
+  );
+};
+
+export default Resumo;
